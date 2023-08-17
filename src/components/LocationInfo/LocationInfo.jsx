97a2cba4cfa69ad692c3c8f62c01aa03d5de530c
GeorgeFlood <mrgeorgeflood@gmail.com>
2023-08-17 05:37:14 +0100

    updated locationInfo.jsx
    I tried to add more tags to my tag list, however the more tags I add it screws with the location tags and doesn't always display the country I want to display.
    
    Unfortunate, but it is what it is.

--- src/components/LocationInfo/LocationInfo.jsx
+++ src/components/LocationInfo/LocationInfo.jsx
@@ -3,40 +3,73 @@ import React, { useEffect, useState } from 'react';
 
 
 const LocationInfo = ({ selectedLocation, details }) => {
-
-  const [imageUrls, setImageUrls] = useState([]);
-
+const [imageUrls, setImageUrls] = useState([]);
+const [isLoading, setIsLoading] = useState(false);
+const [errorMsg, setErrorMsg] = useState('');
   const {country, state, city} = details || {};
 
 const key = 'e13f3ca2ff4d8ec8068ea28ad8950e47';
-const tags = city || state || country + 'popular';
+let tags = city ? `${city}` : state ? `${state}` : `${country}`;
+console.log(tags);
 const url = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${key}&tags=${tags}&format=json&nojsoncallback=1`;
+
+  const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
 
 useEffect(() => {
    if (!selectedLocation) return;
-  fetch(url)
+  setIsLoading(true);
+
+  // Create a new AbortController
+  const controller = new AbortController();
+  const { signal } = controller;
+
+  fetch(url, { signal }) // Pass the signal to the fetch call
     .then(res => res.json())
     .then(data => {
       const urls = data.photos.photo.map(photo => {
         const { farm, server, id, secret } = photo;
         return `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}.jpg`;
       });
-
-      setImageUrls(urls);
+      const selectedUrls = shuffleArray(urls).slice(0, 6);
+      setImageUrls(selectedUrls);
     })
-    .catch(error => console.error('There was an error fetching the photo:', error));
-}, [selectedLocation, details]); 
+    .catch(error => {
+      // Only log the error if it was not an abort error
+      if (error.name !== 'AbortError') {
+        setErrorMsg('There was an error fetching the photo:', error);
+      }
+    });
+   setIsLoading(false);
+  // Return the cleanup function
+  return () => {
+    controller.abort();
+    let tags;
+  };
+}, [selectedLocation, details]);
 
 
 
+console.log(tags)
 
-  return (
-<div>
+return (
+  isLoading ? (
+    <div>Loading...</div>
+  ) : errorMsg ? (
+    <div>{errorMsg}</div>
+  ) : (
+    <div>
       {imageUrls.map((url, index) => (
         <img key={index} src={url} alt={`Image ${index}`} />
       ))}
     </div>
-  );
-};
+  )
+);
 
+      }
 export default LocationInfo;
